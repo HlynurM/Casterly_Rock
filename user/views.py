f19ecc4eebb099907831063f06915bc19e9bc4f2
Aríel Jóhann Árnason <ariel17@ru.is>
2019-05-13 15:29:02 +0000

    wtf eitthva eyddist

--- user/views.py
+++ user/views.py
@@ -4,17 +4,25 @@ from django.shortcuts import render
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from user.forms.profile_form import ProfileForm
+from user.forms.register_form import UserRegisterForm
 from user.models import Profile
+from django.contrib import messages
 
 
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(data = request.POST)
         if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Notandi {username} skráður!')
             form.save()
-            return redirect('login')
+            return redirect('/')
+        else:
+            messages.error(request, f'Villa við skráningu! Vinsamlegast fylgið leiðbeiningum')
+    else:
+        form = UserCreationForm()
     return render(request, 'user/register.html', {
-        'form': UserCreationForm()
+        'form': form
     })
 
 def profile(request):
