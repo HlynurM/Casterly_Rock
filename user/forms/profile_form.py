2221eaed1a1f67e3f636f03dd8e421618cd84c2f
Aríel Jóhann Árnason <ariel17@ru.is>
2019-05-14 17:45:34 +0000

    pre_changes

--- user/forms/profile_form.py
+++ user/forms/profile_form.py
@@ -3,14 +3,14 @@ from user.models import Profile, UserProfile
 
 class ProfileForm(ModelForm):
     class Meta:
-        model = Profile
+        model = UserProfile
         exclude = [ 'id', 'user' ]
         widgets = {
             'profile_image': widgets.TextInput(attrs={ 'class': 'form-control' }),
             'ssn': widgets.TextInput(attrs={ 'class': 'form-control' }),
             'phone': widgets.TextInput(attrs={'class': 'form-control'}),
             'street': widgets.TextInput(attrs={ 'class': 'form-control' }),
-            'region_code': widgets.Select(attrs = { 'class': 'form-control' }),
+            'street.region_code': widgets.Select(attrs = { 'class': 'form-control' }),
             # 'kingdom': widgets.Select(attrs = { 'class': 'form-control' }),
             'estates': widgets.Select(attrs = { 'class': 'form-control' }),
             'bio': widgets.TextInput(attrs={'class': 'form-control'}),
