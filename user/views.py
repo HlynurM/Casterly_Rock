7171c63cae6bc974b68cf775fa375ffc0ede6a86
Aríel Jóhann Árnason <ariel17@ru.is>
2019-05-16 15:19:15 +0000

    reyna laga profile

--- user/views.py
+++ user/views.py
@@ -32,29 +32,33 @@ def register(request):
 
 def profile(request):
 
-    profile = UserProfile.objects.filter(user = request.user).first()
+    #u_profile = UserProfile.objects.filter(user = request.user).first()
     if request.method == 'POST':
         u_form = UserForm(request.POST, instance=request.user)
-        p_form = ProfileForm(request.POST, request.FILES, instance=request.user.userprofile)
+        #p_form = ProfileForm(request.POST, instance=request.user.userprofile)
+        p_form = ProfileForm()
 
-
-        if p_form.is_valid() and u_form.is_valid():
+        #if p_form.is_valid() and u_form.is_valid():
+        if u_form.is_valid() and p_form.is_valid():
             # .save gerir commit by default - en hér uppfærum við user foreign key áður en við committum...
+
             profile = p_form.save(commit = False)
             profile.user = request.user
             profile.save()
-            profile = u_form.save(commit = False)
-            profile.user = request.user
-            profile.save()
+            u_profile = u_form.save(commit = False)
+            u_profile.user = request.user
+            u_profile.save()
             return redirect('profile')
+        else:
+            messages.error(request, f'Villa við skráningu! Vinsamlegast fylgið leiðbeiningum')
 
     else:
         u_form = UserForm(instance = request.user)
-        p_form = ProfileForm(instance = request.user.userprofile)
+        p_form = ProfileForm()
 
 
     return render(request, 'user/profile.html', {
-        'p_form': ProfileForm(instance = profile),
+        'p_form': p_form,
         'u_form': u_form
     })
 
